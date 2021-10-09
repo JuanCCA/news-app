aa3b837e4d6f531ca9c346c80d2c1fcdf22c98d3
Juan Carlos Cantos Agudo <jcantosagudo@gmail.com>
2021-10-10 01:23:03 +0200

    Lists created

--- src/views/Archived.js
+++ src/views/Archived.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {useState} from 'react'
+import ArchivedNew from '../components/ArchivedNew'
 
 const Archived = () => {
+
+    const defaultNews = [{
+        title: 'Titulo 3',
+        description: 'Descripción de la noticia un poco larga',
+        date: 2,//new Date(new Date() - 2222222 ).toString(),
+        content: 'Contenido de la noticia esta si será mas larga hay que tener pensado que puede llegar a ocupar varias líneas',
+        author: 'Pablo Becho Sipape',
+        archiveDate: null
+    },
+    {
+        title: 'Titulo 4',
+        description: 'Descripción de la noticia un poco larga',
+        date: 1,//new Date().toString(),
+        content: 'Contenido de la noticia esta si será mas larga hay que tener pensado que puede llegar a ocupar varias líneas',
+        author: 'Jose María Cantos Sipape',
+        archiveDate: null
+    }]
+
+    const [news, setNews] = useState(defaultNews)
+
     return (
         <>
-           <h1>Archivados</h1> 
+            <p className="display-1 m-auto">Archived</p>
+            <div align="center">
+                <ol>
+                    {
+                        news.map( item => (
+                            <ArchivedNew key={item.date} {...item} />
+                        ))
+                    }
+                </ol>
+            </div>
+            
         </>
     )
 }
 
-Archived.propTypes = {
 
-}
 
 export default Archived
--- src/views/News.js
+++ src/views/News.js
@@ -1,17 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import New from '../components/New'
 
-const News = () => {
+const News = (defaultNews = []) => {
+
+    defaultNews = [{
+        title: 'Titulo 1',
+        description: 'Descripción de la noticia un poco larga',
+        date: 2,//new Date(new Date() - 2222222 ).toString(),
+        content: 'Contenido de la noticia esta si será mas larga hay que tener pensado que puede llegar a ocupar varias líneas',
+        author: 'Pablo Becho Sipape',
+        archiveDate: null
+    },
+    {
+        title: 'Titulo 2',
+        description: 'Descripción de la noticia un poco larga',
+        date: 1,//new Date().toString(),
+        content: 'Contenido de la noticia esta si será mas larga hay que tener pensado que puede llegar a ocupar varias líneas',
+        author: 'Jose María Cantos Sipape',
+        archiveDate: null
+    }]
+
+    const [news, setNews] = useState(defaultNews)
+
+    console.log(news)
+
+
     return (
         <>
-            <h1>Noticias</h1>
+            <p className="display-1 m-auto">Noticias</p>
+            <div align="center">
+                <ol>
+                    {
+                        news.map( item => (
+                            <New key={item.date} {...item} />
+                        ))
+                    }
+                </ol>
+            </div>
             
         </>
     )
 }
 
-News.propTypes = {
 
-}
 
 export default News
